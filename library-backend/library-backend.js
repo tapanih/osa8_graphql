1a6d9622b622ba522226f3e2fcb2a3f8e2655aaf
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-01-02 18:24:31 +0200

    Korjattu kirjojen lisääminen ja syntymäajan muuttaminen

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -193,7 +193,7 @@ const server = new ApolloServer({
   resolvers,
   context: async ({ req }) => {
     const auth = req ? req.headers.authorization : null
-    if (auth && auth.toLowerCase.startsWith("bearer ")) {
+    if (auth && auth.toLowerCase().startsWith("bearer ")) {
       const decodedToken = jwt.verify(
         auth.substring(7), JWT_SECRET
       )
